dd169b8a00a02089f97afe3d98488c7a9768ab65
BuildTools <unconfigured@null.spigotmc.org>
2023-11-13 11:56:19 -0500

    Serve DCV file

--- picky_picker_web/urls.py
+++ picky_picker_web/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
+from django.views.generic import TemplateView
+
 from pickform.views import form_view
+
+DCV_FILENAME = '1C50148033C120998D2A58356A948C73.txt'
 
 urlpatterns = [
     path('form/', form_view, name='form-view'),
+    path(f'.well-known/{DCV_FILENAME}/', TemplateView.as_view(template_name=f'well-known/{DCV_FILENAME}/')),
 ]
