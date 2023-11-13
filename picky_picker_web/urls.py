214f217f2674ddaf0f0040546d0771123a9fe374
BuildTools <unconfigured@null.spigotmc.org>
2023-11-13 11:57:10 -0500

    Remove extraneous slash

--- picky_picker_web/urls.py
+++ picky_picker_web/urls.py
@@ -7,5 +7,5 @@ DCV_FILENAME = '1C50148033C120998D2A58356A948C73.txt'
 
 urlpatterns = [
     path('form/', form_view, name='form-view'),
-    path(f'.well-known/{DCV_FILENAME}/', TemplateView.as_view(template_name=f'well-known/{DCV_FILENAME}/')),
+    path(f'.well-known/{DCV_FILENAME}/', TemplateView.as_view(template_name=f'well-known/{DCV_FILENAME}')),
 ]
